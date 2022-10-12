2e835d9a459f5aee71e067fd93b3bd7a8ee27762
Zev <bennett.zev@gmail.com>
2022-10-12 14:09:10 -0700

    finished the module, not challenge

--- Schema.sql
+++ Schema.sql
@@ -52,3 +52,13 @@ CREATE TABLE titles (
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no, title, from_date)
 );
+
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
